8bee7c55e3d4f580ff10ff84656903e7eea81cfa
Faruq Rasid <me@ruqqq.sg>
2017-02-22 20:54:17 +0800

    Refactor cmd/main.go to only contain utility commands. Sample carbonchain app is now at sample/ folder.

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/ruqqq/blockchainparser"
-	"github.com/ruqqq/blockchainparser/rpc"
 	"github.com/ruqqq/carbonchain"
 	"log"
 	"os"
@@ -17,24 +16,14 @@ func main() {
 	var gomaxprocs int
 	var testnet bool
 	var datadir string
-	var bitcoindIp string
-	var bitcoindPort string
-	var bitcoindRpcUser string
-	var bitcoindRpcPass string
-	var opReturnBtcFee float64
 	flag.BoolVar(&testnet, "testnet", false, "Use testnet")
 	flag.StringVar(&datadir, "datadir", "", "Bitcoin data path")
 	flag.IntVar(&gomaxprocs, "gomaxprocs", -1, "Number of threads to use")
-	flag.StringVar(&bitcoindIp, "rpcip", bitcoindIp, "Bitcoind RPC IP\n\t\t* REQUIRED ONLY FOR STORE COMMAND")
-	flag.StringVar(&bitcoindPort, "rpcport", bitcoindPort, "Bitcoind RPC Port (Default for testnet set to append 1 to this variable)\n\t\t* REQUIRED ONLY FOR store COMMAND")
-	flag.StringVar(&bitcoindRpcUser, "rpcuser", bitcoindRpcUser, "User for bitcoind RPC\n\t\t* REQUIRED ONLY FOR store COMMAND")
-	flag.StringVar(&bitcoindRpcPass, "rpcpassword", bitcoindRpcPass, "Password for bitcoind RPC\n\t\t* REQUIRED ONLY FOR store COMMAND")
-	flag.Float64Var(&opReturnBtcFee, "fee", opReturnBtcFee, "Transaction fee to use\n\t\t* REQUIRED ONLY FOR store COMMAND")
 	flag.BoolVar(&help, "help", help, "Show help")
 	flag.Parse()
 
-	fmt.Printf("testnet: %v\n", testnet)
-	fmt.Printf("datadir: %s\n", datadir)
+	log.Printf("testnet: %v\n", testnet)
+	log.Printf("datadir: %s\n", datadir)
 	args := flag.Args()
 
 	runtime.GOMAXPROCS(gomaxprocs)
@@ -45,8 +34,6 @@ func main() {
 			"Commands:\n"+
 			"  GetHeight\n"+
 			"  GetBlock <hash>\n"+
-			"  Store <data string>\n"+
-			"  Start\n"+
 			"\n"+
 			"Options:\n")
 		flag.PrintDefaults()
@@ -93,30 +80,6 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Printf("Next Block: %x\n", blockchainparser.ReverseHex(nextHash))
-
-		return
-	} else if len(args) > 0 && args[0] == "Store" {
-		options := &rpc.RpcOptions{
-			Host:    bitcoindIp,
-			Port:    bitcoindPort,
-			User:    bitcoindRpcUser,
-			Pass:    bitcoindRpcPass,
-			Testnet: testnet,
-		}
-		_, err := carbonchain.Store([]byte(args[1]), 0xfa, opReturnBtcFee, options)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else if len(args) > 0 && args[0] == "Start" {
-		err = cc.Init()
-		if err != nil {
-			panic(err)
-		}
-
-		err = cc.Watch()
-		if err != nil {
-			panic(err)
-		}
 
 		return
 	}
---
+++ sample/app.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"github.com/lunixbochs/struc"
+	"github.com/ruqqq/blockchainparser/rpc"
+	"github.com/ruqqq/carbonchain"
+	"log"
+	"os"
+	"runtime"
+)
+
+func main() {
+	var help bool
+	var gomaxprocs int
+	var testnet bool
+	var datadir string
+	var bitcoindIp string
+	var bitcoindPort string
+	var bitcoindRpcUser string
+	var bitcoindRpcPass string
+	var opReturnBtcFee float64
+	flag.BoolVar(&testnet, "testnet", false, "Use testnet")
+	flag.StringVar(&datadir, "datadir", "", "Bitcoin data path")
+	flag.IntVar(&gomaxprocs, "gomaxprocs", -1, "Number of threads to use")
+	flag.StringVar(&bitcoindIp, "rpcip", bitcoindIp, "Bitcoind RPC IP\n\t\t* REQUIRED ONLY FOR STORE COMMAND")
+	flag.StringVar(&bitcoindPort, "rpcport", bitcoindPort, "Bitcoind RPC Port (Default for testnet set to append 1 to this variable)\n\t\t* REQUIRED ONLY FOR store COMMAND")
+	flag.StringVar(&bitcoindRpcUser, "rpcuser", bitcoindRpcUser, "User for bitcoind RPC\n\t\t* REQUIRED ONLY FOR store COMMAND")
+	flag.StringVar(&bitcoindRpcPass, "rpcpassword", bitcoindRpcPass, "Password for bitcoind RPC\n\t\t* REQUIRED ONLY FOR store COMMAND")
+	flag.Float64Var(&opReturnBtcFee, "fee", 0, "Transaction fee to use\n\t\t* REQUIRED ONLY FOR store COMMAND")
+	flag.BoolVar(&help, "help", help, "Show help")
+	flag.Parse()
+
+	log.Printf("testnet: %v\n", testnet)
+	log.Printf("datadir: %s\n", datadir)
+	wd, _ := os.Getwd()
+	log.Printf("app datadir: %s\n", wd)
+	args := flag.Args()
+
+	runtime.GOMAXPROCS(gomaxprocs)
+	log.Printf("GOMAXPROCS: %d\n", runtime.GOMAXPROCS(-1))
+
+	showHelp := func() {
+		fmt.Fprint(os.Stderr, "Sample Carbonchain App\n(c)2017 Faruq Rasid\n\n"+
+			"Commands:\n"+
+			"  Store <data string>\n"+
+			"  Start\n"+
+			"\n"+
+			"Options:\n")
+		flag.PrintDefaults()
+	}
+
+	if len(args) == 0 || help {
+		showHelp()
+		return
+	}
+
+	if len(args) > 0 && args[0] == "Store" {
+		options := &rpc.RpcOptions{
+			Host:    bitcoindIp,
+			Port:    bitcoindPort,
+			User:    bitcoindRpcUser,
+			Pass:    bitcoindRpcPass,
+			Testnet: testnet,
+		}
+		var data []byte
+		if len(args) == 1 {
+			reader := bufio.NewReader(os.Stdin)
+			log.Printf("Enter string data: ")
+			input, _ := reader.ReadString('\n')
+			data = []byte(input[:len(input)-1])
+		} else {
+			data = []byte(args[1])
+		}
+		_, err := carbonchain.Store(data, 0xfa, opReturnBtcFee, options)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return
+	} else if len(args) > 0 && args[0] == "Start" {
+		cc, err := carbonchain.NewCarbonChainWithDb(&carbonchain.CarbonChainOptions{LogLevel: carbonchain.LOG_LEVEL_DEBUG, Testnet: testnet, DataDir: datadir, PacketId: 0xfa, ProcessFunc: ProcessDatapack})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = cc.Init()
+		if err != nil {
+			panic(err)
+		}
+
+		err = cc.Watch()
+		if err != nil {
+			panic(err)
+		}
+
+		return
+	}
+}
+
+func ProcessDatapack(cc *carbonchain.CarbonChain, carbonDb *bolt.DB) {
+	datapacks := make([]carbonchain.Datapack, 0)
+	datapackIds := make(map[int][]byte)
+
+	// Get all available datapacks
+	err := carbonDb.View(func(tx *bolt.Tx) error {
+		bDatas := tx.Bucket([]byte("datas"))
+
+		c := bDatas.Cursor()
+		for i, datapackByte := c.First(); i != nil; i, datapackByte = c.Next() {
+			buf := bytes.NewBuffer(datapackByte)
+			datapack := carbonchain.Datapack{}
+			err := struc.Unpack(buf, &datapack)
+			if err != nil {
+				return err
+			}
+
+			datapacks = append(datapacks, datapack)
+			datapackIds[len(datapacks)-1] = i
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Consume datapacks
+	if len(datapacks) > 0 {
+		fmt.Printf("Datapacks (%d):\n", len(datapacks))
+		for _, datapack := range datapacks {
+			blockHash, err := cc.GetTransactionBlockHash(datapack.FirstTxId)
+			if err != nil {
+				log.Fatal(err)
+			}
+			confirmations, err := cc.GetBlockConfirmation(blockHash)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("\t[%s (c: %d)] %s\n", datapack.OutputAddr, confirmations, datapack.Data)
+		}
+
+		// Delete the datapacks after consume
+		err = carbonDb.Batch(func(tx *bolt.Tx) error {
+			bDatas := tx.Bucket([]byte("datas"))
+			for i := 0; i < len(datapacks); i++ {
+				bDatas.Delete(datapackIds[i])
+			}
+
+			return nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
